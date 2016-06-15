a59f92b5579cf893148ca4288692d52662e8e019
darkjoker <darkjoker93@gmail.com>
2016-06-15 11:52:46 +0200

    hopefully js wont fuck up - R9

--- js/reservations.js
+++ js/reservations.js
@@ -6,48 +6,46 @@ $(document).ready(function() {
     }
 
     function updateTable () {
-        console.log(timestamp);
         $.ajax('getStuff.php?t=' + timestamp)
         .done(function(data) {
             var insertList = $.each(data.insertList, function (k,v) { v.type = 'insert'; });
             var deleteList = $.each(data.deleteList, function (k,v) { v.type = 'delete'; });
-            var list = insertList.concat(deleteList).sort(ordTimestamp);
-            console.log(list);
-            $.each(list,function(key,obj) {
-                timestamp = data.timestamp; // only update timestamp when there's actually something (in case it misses something for some reason!
-                if (obj.type == 'insert') {
-                    var el = $("<div class = 'list_row book_row' id = 'book_"+obj.mid+"_"+obj.start+"'>"+
-                               "<span class = 'list_cell'>"+obj.machineName+"</span>"+
-                               "<span class = 'list_cell'>"+obj.start+"</span>"+
-                               "<span class = 'list_cell'>"+obj.end+"</span>"+
-                               "<span class = 'list_cell'>"+obj.duration+"</span>"+
-                               "</div>");
-                    var done = false;
-                    $('.book_row').each(function() {
-                        var m = this.id.match(/^book_\d+_(\d{2}:\d{2})$/);
-                        console.log(m[1] < obj.start);
-                        if (!done && obj.start < m[1]) {
-                            // found the first one greater than this one!
-                            $(this).before(el);
-                            done = true;
-                        }
-                    });
-                    if (done == false) {
-                        if ($('.book_row').length > 0) {
-                            $('.book_row').last().after(el);
-                        }
-                        else {
-                            $('.head_row').last().after(el);
+            if (insertList !== undefined && deleteList !== undefined) {
+                var list = insertList.concat(deleteList).sort(ordTimestamp);
+                $.each(list,function(key,obj) {
+                    timestamp = data.timestamp; // only update timestamp when there's actually something (in case it misses something for some reason!
+                    if (obj.type == 'insert') {
+                        var el = $("<div class = 'list_row book_row' id = 'book_"+obj.mid+"_"+obj.start+"'>"+
+                                "<span class = 'list_cell'>"+obj.machineName+"</span>"+
+                                "<span class = 'list_cell'>"+obj.start+"</span>"+
+                                "<span class = 'list_cell'>"+obj.end+"</span>"+
+                                "<span class = 'list_cell'>"+obj.duration+"</span>"+
+                                "</div>");
+                        var done = false;
+                        $('.book_row').each(function() {
+                            var m = this.id.match(/^book_\d+_(\d{2}:\d{2})$/);
+                            if (!done && obj.start < m[1]) {
+                                // found the first one greater than this one!
+                                $(this).before(el);
+                                done = true;
+                            }
+                        });
+                        if (done == false) {
+                            if ($('.book_row').length > 0) {
+                                $('.book_row').last().after(el);
+                            }
+                            else {
+                                $('.head_row').last().after(el);
+                            }
                         }
                     }
-                }
-                else if (obj.type == 'delete') {
-                    // delete
-                    // jQuery doesn't handle colons well
-                    console.log('remove! book_'+obj.mid+'_'+obj.start);
-                    $(document.getElementById('book_'+obj.mid+'_'+obj.start)).remove();
-                }
-            });
+                    else if (obj.type == 'delete') {
+                        // delete
+                        // jQuery doesn't handle colons well
+                        $(document.getElementById('book_'+obj.mid+'_'+obj.start)).remove();
+                    }
+                });
+            }
 
         });
 
